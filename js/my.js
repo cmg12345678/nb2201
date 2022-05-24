e7b971f417234462d04f972b44f5b6e088f65947
王振威 <1398732407@qq.com>
2022-05-24 11:59:47 +0800

    'wzw提交'

--- js/my.js
+++ js/my.js
@@ -1,6 +1,7 @@
 window.onload = function show() {
     var ajax_ = new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
     ajax_.open('get', 'http://localhost:3000/report/new');
+
     ajax_.send();
     ajax_.onreadystatechange = function () {
         if (ajax_.readyState == 4) {
@@ -13,18 +14,6 @@ window.onload = function show() {
 }
 
 
-//  渲染数据
-// function render(data_) {
-//     console.log(data_);
-//     var str = '';
-//     for (var item of data_) {
-//         str += '<li>';
-//         str += '<p>' + item.imgtitle + '</p>';
-//         str += '<img src="' + item.imgurl + '">'
-//         str += '<p>' + item.note + '</p>';
-//         str += '</li>';
-//     }
-// }
 
 function render(data_) {
     var str = '';
